db0d89339f68d7be856e6f23c2e814567b84d245
Hainizam <Ingeniworks@Hainizams-Mac-mini.local>
2017-10-03 17:55:46 +0800

    added login button function

--- MardiKomunitiIOS/MainTabBarVC.swift
+++ MardiKomunitiIOS/MainTabBarVC.swift
@@ -14,6 +14,7 @@ class MainTabBarVC: UITabBarController {
         super.viewDidLoad()
 
         configureNavigationBar()
+        configureTabbar()
     }
     
     func configureNavigationBar() {
@@ -21,8 +22,35 @@ class MainTabBarVC: UITabBarController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Login", style: .done, target: self, action: #selector(loginButtonTapped(_:)))
     }
     
+    func configureTabbar() {
+        
+        let attributes: [String : Any] = [ NSFontAttributeName: UIFont(name: "Futura-Bold", size: 12.0)!]
+        
+        for bar in tabBar.items! {
+            
+            bar.titlePositionAdjustment = UIOffsetMake(0, -(tabBar.frame.height - 12) / 2)
+            bar.setTitleTextAttributes(attributes, for: .normal)
+        }
+    }
+    
     @objc private func loginButtonTapped(_ sender : UIBarButtonItem) {
         
         print("Tapped")
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        var base = appDelegate.window?.rootViewController
+        base = self.selectedViewController
+        
+        if let base = base as? PengumumanTVC {
+            base.performSegue(withIdentifier: "GOTO_LOGIN", sender: base)
+        }
+        
+        if let base = base as? AplikasiBerkaitanCVC {
+            base.performSegue(withIdentifier: "GOTO_LOGIN", sender: base)
+        }
     }
 }
+
+
+
+
+
